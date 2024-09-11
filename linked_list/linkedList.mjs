c54dc29aca7df350b4c046230ebf7a0b8de2c982
larryp89 <42215520+larryp89@users.noreply.github.com>
2024-09-11 08:52:27 +0100

    Created Node & LinkedList class. Com pleted append/prepend/size/getHead/getTail/at(index) methods

---
+++ linked_list/linkedList.mjs
@@ -0,0 +1,100 @@
+class Node {
+  constructor(value = null) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  // Add to end of the list
+  append(value) {
+    const newNode = new Node(value);
+
+    // If the head is null, the list is empty so set both
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+
+    // Otherwise, the current tail is a node which needs to point to the new node
+    this.tail.nextNode = newNode;
+    // Set the new node as the tail
+    this.tail = newNode;
+    return;
+  }
+
+  // Add to the start of the list
+  prepend(value) {
+    const newNode = new Node(value);
+
+    // If the head is null, the list is empty so set both
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+
+    // Otherwise, the current head is a node which the newnode must point
+    newNode.nextNode = this.head;
+    // Set this node to head
+    this.head = newNode;
+    return;
+  }
+
+  // Return number of nodes in the list
+  size() {
+    // If the the list is empty return 0
+    if (this.head === null) return 0;
+
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      counter += 1;
+      currentNode = currentNode.nextNode;
+    }
+    return counter;
+  }
+
+  // Returns head
+  getHead() {
+    return {
+      headValue: this.head ? this.head.value : null,
+    };
+  }
+
+  // Returns tails
+  getTail() {
+    return {
+      tailValue: this.tail ? this.tail.value : null,
+    };
+  }
+
+  // Returns node at a given index
+  at(index) {
+    // Prevent accessing negative index
+    if (index < 0) return null;
+
+    // If there is no list, return false
+    if (this.head === null) return null;
+
+    // Otherwise iterate through
+    let currentNode = this.head;
+    let counter = 0;
+
+    while (currentNode !== null) {
+      // If the counter number equals the index, return the current node
+      if (counter === index) return currentNode;
+      //Otherwise increase the counter and go to next node
+      currentNode = currentNode.nextNode;
+      counter++;
+    }
+    return null;
+  }
+}
